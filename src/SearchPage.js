333e834a0fcb16bcc302754d858d98c4e99117ad
inesquecive7 <51125561+inesquecive7@users.noreply.github.com>
2019-09-28 20:15:38 +0100

    Fixed search field

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -5,18 +5,22 @@ class SearchPage extends Component {
     render() {
         return (
             <div>
-                <div className="search-books-input-wrapper">
-                {/*
+                <div className = "search-books">
+                   <div className = "search-books-bar">
+                   <Link to="/" className="close-search">Close</Link>   
+                    <input type="text" placeholder="Search by title or author"/> 
+                        <div className="search-books-input-wrapper">
+                            {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
                   You can find these search terms here:
                   https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-          </div>
-          <Link to="/" className="close-search">Close</Link>             
+                             */}
+                        </div>         
+                    </div>
+                </div>
             </div>
         );
     }
